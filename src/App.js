044ea2b02f4fe7f6310598a7a46a2f8168b3f8f7
Saymiki <mikaela.montesclaros@gmail.com>
2023-07-13 01:06:29 -0500

    header and app.js updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   // gql, 
   // useQuery 
 } from '@apollo/client';
+import Header from './components/Header/Header';
 import loginForm from './components/pages/loginForm';
 import signUpForm from './components/pages/signUpForm';
 
@@ -16,100 +17,14 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
+      <Header />
+      <div className="App">
       <h1>Food Friendz</h1>
       {/* Add your components and routes here */}
     </div>
+    </ApolloProvider>
   );
 }
 
 export default App; 
-
-
-
-
-
-// const client = new ApolloClient({
-//   uri: 'https://foodie-friends-server-b0943e90cb47.herokuapp.com/', // Replace with your server's URL
-//   cache: new InMemoryCache(),
-// });
-
-
-// const GET_EVENTS = gql`
-//   query GetEvents {
-//     events {
-//       _id
-//       title
-//       description
-//     }
-//   }
-// `;
-
-// const GET_RESTAURANTS = gql`
-//   query GetRestaurants {
-//     restaurants {
-//       _id
-//       name
-//       description
-//     }
-//   }
-// `;
-
-// const GET_USERS = gql`
-//   query GetUsers {
-//     users {
-//       _id
-//       firstName
-//       lastName
-//     }
-//   }
-// `;
-
-// function App() {
-//   const { loading: loadingEvents, error: errorEvents, data: dataEvents } = useQuery(GET_EVENTS);
-//   const { loading: loadingRestaurants, error: errorRestaurants, data: dataRestaurants } = useQuery(GET_RESTAURANTS);
-//   const { loading: loadingUsers, error: errorUsers, data: dataUsers } = useQuery(GET_USERS);
-
-//   if (loadingEvents || loadingRestaurants || loadingUsers) return <p>Loading...</p>;
-//   if (errorEvents || errorRestaurants || errorUsers) return <p>Error: {errorEvents?.message || errorRestaurants?.message || errorUsers?.message}</p>;
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Events</h1>
-//         {dataEvents.events.map((event) => (
-//           <div key={event._id}>
-//             <h3>{event.title}</h3>
-//             <p>{event.description}</p>
-//           </div>
-//         ))}
-
-//         <h1>Restaurants</h1>
-//         {dataRestaurants.restaurants.map((restaurant) => (
-//           <div key={restaurant._id}>
-//             <h3>{restaurant.name}</h3>
-//             <p>{restaurant.description}</p>
-//           </div>
-//         ))}
-
-//         <h1>Users</h1>
-//         {dataUsers.users.map((user) => (
-//           <div key={user._id}>
-//             <h3>{user.firstName} {user.lastName}</h3>
-//           </div>
-//         ))}
-//       </header>
-//     </div>
-//   );
-// }
-
-// function AppWrapper() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <App />
-//     </ApolloProvider>
-//   );
-// }
-
-// export default AppWrapper;
-
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import './Header.css';
 
-import Auth from '../../utils/auth';
+export default function Header() {
+  return (
+    <>
+      <div className="relative bg-teal-700">
+        <div className="absolute inset-0">
+          <img
+            className="w-full h-full object-cover object-center"
+            src="https://cdn.choosechicago.com/uploads/2019/06/AA_TASTE_04-900x605.jpeg"
+            alt=""
+          />
+          <div className="absolute inset-0" aria-hidden="true" />
+        </div>
+        <div className="relative text-center max-w-7xl mx-auto py-24 px-4 flex flex-col items-center sm:py-32 sm:px-6 lg:px-8">
+          <h1 className="header text-4xl font-extrabold tracking-tight text-stone-50 sm:text-5xl lg:text-6xl">Foodie Friends</h1>
+          <p className="about-blurb mt-6 text-xl font-medium xl:text-2xl text-stone-50 max-w-3xl">
+            Welcome 
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
 
-const Header = () => {
-    const logout = (event) => {
-        event.preventDefault();
-        Auth.logout();
-    };
-    return (
-        <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-            <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-                <Link className="text-dark" to="/">
-                    <h1 className="m-0" style={{ fontSize: '3rem' }}>
-                        Tech Friends
-                    </h1>
-                </Link>
-                <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-                    Meet your new programming pals.
-                </p>
-                <div>
-                    {Auth.loggedIn() ? (
-                        <>
-                            <Link className="btn btn-lg btn-primary m-2" to="/me">
-                                View My Profile
-                            </Link>
-                            <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                                Logout
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            <Link className="btn btn-lg btn-primary m-2" to="/login">
-                                Login
-                            </Link>
-                            <Link className="btn btn-lg btn-light m-2" to="/signup">
-                                Signup
-                            </Link>
-                        </>
-                    )}
-                </div>
-            </div>
-        </header>
-    );
-};
 
-export default Header;
+// 
+// 
+
+// BELOW IS PREV CODE
+// Link buttons can be leveraged once the pages for event, sign up, login, etc are mades
+
+// 
+// 
+// 
+
+// import React from 'react';
+
+
+// const Header = () => {
+//     const logout = (event) => {
+//         event.preventDefault();
+//         Auth.logout();
+//     };
+//     return (
+//         <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+//             <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+//                 <Link className="text-dark" to="/">
+//                     <h1 className="m-0" style={{ fontSize: '3rem' }}>
+//                         Tech Friends
+//                     </h1>
+//                 </Link>
+//                 <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+//                     Meet your new programming pals.
+//                 </p>
+//                 <div>
+//                     {Auth.loggedIn() ? (
+//                         <>
+//                             <Link className="btn btn-lg btn-primary m-2" to="/me">
+//                                 View My Profile
+//                             </Link>
+//                             <button className="btn btn-lg btn-light m-2" onClick={logout}>
+//                                 Logout
+//                             </button>
+//                         </>
+//                     ) : (
+//                         <>
+//                             <Link className="btn btn-lg btn-primary m-2" to="/login">
+//                                 Login
+//                             </Link>
+//                             <Link className="btn btn-lg btn-light m-2" to="/signup">
+//                                 Signup
+//                             </Link>
+//                         </>
+//                     )}
+//                 </div>
+//             </div>
+//         </header>
+//     );
+// };
+
+// export default Header;
